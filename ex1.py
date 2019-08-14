4b1e3cf43ca83323c36b30d0b2eca2de1341647c
sherikopec <sherikopec@gmail.com>
2019-08-14 20:11:49 +0800

    initial commit

---
+++ ex1.py
@@ -0,0 +1,53 @@
+# Lists
+
+name = 'hayley'
+level = 5
+awesomeness = 95.5
+is_raining = True
+
+# print(name[0])
+
+backpack = []
+
+backpack = [
+    'drink bottle',
+    'yoghurt',
+    'headphones',
+    'laptop'
+]
+
+# backpack.append('blueberries')
+
+# print(backpack[1])
+
+# backpack[0] = 'lunchbox'
+# print(backpack)
+
+# #These do the same thing, but the first is simpler
+# for index, item in enumerate(backpack):
+#     print(index, item)
+
+# counter = 0
+# for item in backpack:
+#     print(counter, item)
+#     counter = counter + 1
+
+# while loop
+
+# counter = 0
+# while counter < len(backpack):
+#     print(counter, backpack[counter])
+#     counter = counter + 1
+
+# for num in range(1,11):
+#     print(num)
+    
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num = num + 1  #num += 1
+
+# tuples are immutable
+
+# person = ('hayley', 'awesome')
+# print(person)
---
+++ ex2.py
@@ -0,0 +1,27 @@
+## TASK 2
+​
+# given a 5x5 grid, add the last column and row, then flip the card at the
+# coordinate specified by the user
+​
+five_by_five_grid = [[
+    'X','0','X','X','X',
+    'X','X','0','0','0',
+    'X','0','X','0','X',
+    '0','X','X','X','X',
+    'X','0','0','X','X',
+]]
+​
+# first step is to add colum 6 and row 6
+​
+# output the grid to the user
+# ask the user for the coordinate of the card to flip
+# e.g. input could be: (0,2)
+​
+# output the grid with the flipped card
+​
+## TASK 2
+​
+# given a six by six grid, work out what card was flipped
+​
+# your program should output the coordinate of the flipped card
+# in this case it would be: (x,y)
